c0b9096fbe432bd4bb633d267efe02f5e26e4cd9
tdragon <1210261+tdragon@users.noreply.github.com>
2025-02-07 22:11:55 +0200

    feat(keyboard): add home row mods and mouse key configuration
    - Add home row mods for ASDFG and HJKL keys
    - Configure mouse key interval and delay parameters

--- keyboards/monsgeek/m1/keymaps/tdragon/keymap.c
+++ keyboards/monsgeek/m1/keymaps/tdragon/keymap.c
@@ -21,6 +21,19 @@ enum __layers { BASE, SYMB, FUNC, DIGI, MAC_W, MAC_FN };
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
+// Home roe mod
+#define HR_A LCTL_T(KC_A)
+#define HR_S LALT_T(KC_S)
+#define HR_D LSFT_T(KC_D)
+#define HR_F LGUI_T(KC_F)
+#define HR_G LAG_T(KC_G)
+
+#define HR_H LAG_T(KC_H)
+#define HR_J RGUI_T(KC_J)
+#define HR_K RSFT_T(KC_K)
+#define HR_L RALT_T(KC_L)
+#define HR_SCLN RCTL_T(KC_SCLN)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -28,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        MO(DIGI),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
+        MO(DIGI),HR_A,    HR_S,    HR_D,    HR_F,    HR_G,    HR_H,    HR_J,    HR_K,    HR_L,    HR_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LALT, KC_LGUI,                   LT(SYMB,KC_SPC),                    KC_RALT, KC_RCTL, MO(FUNC),         KC_LEFT, KC_DOWN, KC_RGHT),
 
